7018e197dcd08cae51476918f464a8274c5185ad
azerg <pub@azerg.com>
2015-02-04 16:54:24 +0200

    refactoring

--- src/NppBplistPlugin/src/PluginDefinition.h
+++ src/NppBplistPlugin/src/PluginDefinition.h
@@ -28,7 +28,7 @@
 //-------------------------------------//
 // Here define your plugin name
 //
-const TCHAR NPP_PLUGIN_NAME[] = TEXT("Notepad++ plist plugin");
+const TCHAR NPP_PLUGIN_NAME[] = TEXT("Notepad++ bplist plugin");
 
 //-----------------------------------------------//
 //-- STEP 2. DEFINE YOUR PLUGIN COMMAND NUMBER --//
